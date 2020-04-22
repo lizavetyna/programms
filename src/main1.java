3dc5248a1dc963154631a751056f2d4d41571643
lizavetyna <troshchilova.ea@phystech.edu>
2020-04-22 19:40:02 +0300

    Commit 2

---
+++ src/main1.java
@@ -0,0 +1,21 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package PACKAGE_NAME;
+
+import javax.swing.JFrame;
+
+public class Main1 {
+    public Main1() {
+    }
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Java Rase");
+        f.setDefaultCloseOperation(3);
+        f.setSize(1100, 600);
+        f.add(new Road());
+        f.setVisible(true);
+    }
+}
